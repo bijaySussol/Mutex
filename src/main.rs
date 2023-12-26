7d49e6700eee6b538174f5b88c7d251651b52c0a
bijaySussol <bijaypunmagar@gmail.com>
2023-12-26 11:38:05 +0000

    logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use tokio::time::{sleep, Duration};
+
+async fn person(remote_arc: Arc<Mutex<i32>>, name: String, new_channel: i32) {
+    // request access to the remote
+    let mut real_remote = remote_arc.lock().await;
+
+    // Changes tv channel
+    *real_remote = new_channel;
+    println!("{} changed the channel", name);
+    println!("Watching channel: {}\n", new_channel);
+
+    sleep(Duration::from_secs(5)).await;
+}
+
+#[tokio::main]
+async fn main() {
+    let tv_channel = 10;
+    let remote = Mutex::new(tv_channel);
+    let remote_arc = Arc::new(remote);
+
+    let mut task_handles = Vec::new();
+    for (name, new_channel) in [("Bijay", 11), ("Bimala", 32), ("Shanaya", 43)] {
+        task_handles.push(tokio::spawn(person(
+            remote_arc.clone(),
+            name.to_string(),
+            new_channel,
+        )));
+    }
+
+    for handle in task_handles {
+        handle.await.unwrap();
+    }
 }
